ecc5b3f001be2af6995e56d2277ca26c86045d39
Rahmat Ardiansyah <rahmat21@student.uir.ac.id>
2020-10-25 18:46:42 +0700

    belajar perulangan dan pengkondisian

---
+++ pertemuan3/latihan1.5.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>latihan 1.5</title>
+	<style type="text/css">
+		.warna-baris{
+			background-color: silver;
+		}
+	</style>
+</head>
+<body>
+	<table border="1" cellpadding="10" cellspacing="0">
+		<!-- <?php 
+			for ($i=1; $i <= 3; $i++) { 
+				echo "<tr>";
+				for ($j=1; $j <= 5; $j++) { 
+					echo "<td>$i,$j</td>";
+				}
+				echo "</tr>";
+			}
+		?> -->
+
+		<!-- syntax template -->
+		<?php for ($i=1; $i <= 5; $i++) : ?>
+			<?php if ($i % 2 == 1): ?>
+			<tr class="warna-baris">
+			<?php else: ?>
+			<tr>
+			<?php endif; ?>
+				<?php for ($j=1; $j <= 5; $j++) : ?>
+					<td><?php echo "$i,$j"; ?></td>
+				<?php endfor; ?>
+			</tr>
+		<?php endfor; ?>
+	</table>
+</body>
+</html>

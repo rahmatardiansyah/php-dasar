5f8d5eb5b4f57b2f31aa23990a31421668ce96d7
Rahmat Ardiansyah <rahmat21@student.uir.ac.id>
2020-10-26 22:00:51 +0700

    menampilkan data mahasiswa lebih modular

---
+++ pertemuan8/functions.php
@@ -0,0 +1,18 @@
+<?php 
+
+//buat koneksi
+$koneksi = mysqli_connect("localhost","rahmat","rahmat21","phpdasar");
+
+function query($query)
+{
+	global $koneksi;
+	$result = mysqli_query($koneksi, $query);
+	$rows = [];
+	while ($row = mysqli_fetch_assoc($result)) {
+		$rows[] = $row;
+	}
+	return $rows;
+
+}
+
+ ?>
---
+++ pertemuan8/index2.php
@@ -0,0 +1,45 @@
+<?php 
+	require 'functions.php';
+
+	$mahasiswa = query("SELECT * FROM mahasiswa");
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Halaman Admin</title>
+</head>
+<body>
+	<h1>Daftar Mahasiswa</h1>
+
+	<table border="1" cellpadding="10" cellspacing="0">
+		<tr>
+			<th>No.</th>
+			<th>Aksi</th>
+			<th>Gambar</th>
+			<th>NPM</th>
+			<th>Nama</th>
+			<th>Email</th>
+			<th>Jurusan</th>
+		</tr>
+		<?php $i = 1; ?>
+		<?php foreach ($mahasiswa as $rows): ?>
+			<tr>
+				<td><?php echo $i; ?></td>
+				<td>
+					<a href="">Ubah</a> |
+					<a href="">hapus</a>
+				</td>
+				<td><img src="img/<?php echo $rows["gambar"]; ?>" alt="" width="100px" height="100px"></td>
+				<td><?php echo $rows["npm"]; ?></td>
+				<td><?php echo $rows["nama"]; ?></td>
+				<td><?php echo $rows["email"]; ?></td>
+				<td><?php echo $rows["jurusan"]; ?></td>
+			</tr>
+		<?php $i++; ?>
+		<?php endforeach ?>
+
+	</table>
+</body>
+</html>

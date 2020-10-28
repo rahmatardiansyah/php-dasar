671baac58aa10f0ecefd479610f81405b86ecc2f
Rahmat Ardiansyah <rahmat21@student.uir.ac.id>
2020-10-28 21:35:23 +0700

    insert data

---
+++ pertemuan9/functions.php
@@ -0,0 +1,46 @@
+<?php 
+
+//buat koneksi
+$koneksi = mysqli_connect("localhost","rahmat","rahmat21","phpdasar");
+
+function query($query)
+{
+	global $koneksi;
+	$result = mysqli_query($koneksi, $query);
+	$rows = [];
+	while ($row = mysqli_fetch_assoc($result)) {
+		$rows[] = $row;
+	}
+	return $rows;
+
+}
+
+function tambah($data){
+
+		global $koneksi;//membuat variabel koneksi menjadi global
+		//ambil data dari tiap element dalam form
+		// simpan dulu ke dalam variable supaya saat query dibuat tidak kesulitan.
+		// jika dimakssukan ke fucntions tidak lagi menggunakan variabel $_POST tetapi menggunakan variabel argumen/parameter yg dikirim yaitu $data
+		$nama = $data["nama"];
+		$npm = $data["npm"];
+		$email = $data["email"];
+		$jurusan = $data["jurusan"];
+		$gambar = $data["gambar"];
+
+		//query untuk insert data
+		$query = "INSERT INTO mahasiswa VALUES ('','$nama','$npm','$email','$jurusan','$gambar')";
+		mysqli_query($koneksi, $query);
+
+		// kembalikan nilai query berhasil atau tidak
+		return mysqli_affected_rows($koneksi);
+}
+		// cek apakah data berhasil ditambahkan atau tidak
+		// if (mysqli_affected_rows($koneksi) > 0) {
+		// 	echo "berhasil";
+		// }else{
+		// 	echo "gagal";
+		// 	echo "<br>";
+		// 	echo mysqli_error($koneksi);
+		// }
+
+ ?>

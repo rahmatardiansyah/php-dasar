54223dd18fc04f395fc2861f0b97036af825ab3f
Rahmat Ardiansyah <rahmat21@student.uir.ac.id>
2020-11-05 19:48:24 +0700

    membuat fitur login'Masih bisa ngakses index.php tanpa login'

---
+++ pertemuan14/hapus.php
@@ -0,0 +1,20 @@
+<?php 
+	
+	require 'functions.php';
+	
+	// tangkap id nya dulu dengan $_GET karena id dikirim menggunakan url
+	$id = $_GET["id"];
+
+	// cek apakah ada baris yang berubah atau tidak -1/1
+	if(hapus($id) > 0){
+		echo "<script>
+			alert('data berhasil dihapus!!');
+			document.location.href = 'index.php';
+			</script>";
+	}else{
+		echo "<script>
+			alert('data gagal dihapus!');
+			document.location.href = 'index.php';
+			</script>";
+	}
+ ?>
